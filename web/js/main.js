2d3482fbf6ff922039ab63eff3435c0f1be2cd06
Matt Cooper <mattc@xbox.com>
2015-06-24 12:50:09 -0700

    correctly authing to the service, but jQuery is treating 302 as an error

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 	$("#fetchWIs").click(function() {
-		alert("TODO!");
+		var vsoUri = "https://" + $("#accountName").val() + ".visualstudio.com/DefaultCollection/_apis/projects?api-version=1.0";
+		var paToken = $("#personalAccessToken").val();
+		console.log("Calling: " + vsoUri);
+		$.ajax({
+			"method": "GET",
+			"url": vsoUri,
+			"crossDomain": true,
+			"dataType": "json",
+			"jsonp": false,
+			"headers": {
+				"Authorization": "Basic " + paToken
+			}
+		}).then(function(data){
+			console.log("Got: " + data);
+			alert("Got: " + data);
+		}).fail(function(jqxhr, status, err){
+			console.error("Whoops: " + status);
+			console.error(err);
+		}).always(function() {
+			console.log("done trying");
+		});
 	});
 });
